034569b7af686959e6e49a683f97176726b55010
janetchang <janetlaichang@gmail.com>
2013-05-08 14:20:10 -0700

    attempt user authentication without clearance

--- app/models/sp_user.rb
+++ app/models/sp_user.rb
@@ -1,17 +1,50 @@
 class SpUser
+
+  Clearance::User
   include HTTParty
-    base_uri 'staging.sharepractice.com'
-    # if Rails.env == "production"
-    #   base_uri 'api.sharepractice.com' #ENV["API_RESOURCE_URL"]
-    # elsif Rails.env == "development" 
-    #   base_uri 'staging.sharepractice.com'
-    # else #test
-    #   base_uri 'localhost:3000'
-    # end
-    default_params :output => 'json'
-    format :json
+  base_uri 'staging.sharepractice.com'
+  # if Rails.env == "production"
+  #   base_uri 'api.sharepractice.com' #ENV["API_RESOURCE_URL"]
+  # elsif Rails.env == "development" 
+  #   base_uri 'staging.sharepractice.com'
+  # else #test
+  #   base_uri 'localhost:3000'
+  # end
+  default_params :output => 'json'
+  format :json
+  
+  attr_accessible :email, :first_name, :last_name, :npi, :phone, :degree, :specialty, :website, :location, :history, :verified, :avatar_url
+
+  def initialize(email, password)
+    response = get('/api/v1/tokens?format=json', :query = {:email => email, :password => password})
+    @email =      email
+    @password =   password
+    @first_name = response[:first_name]
+    @last_name =  response[:last_name]
+    @npi =        response[:npi]
+    @phone =      response[:phone]
+    @degree =     response[:degree]
+    @specialty =  response[:specialty]
+    @website =    response[:website]
+    @location =   response[:location]
+    @history =    response[:history]
+    @verified =   response[:verified]
+    @avatar_url = response[:avatar_url]
+  end
 
   def self.find_by_id(id)
     get('/api/v1/users/', :query => {:id => id})
+  end
+
+  def self.find_by_email(email)
+    get('/api/v1/users/', :query => {:email => email})
+  end
+
+  def self.find_followers_of(user_id)
+    get('api/v1/colleagues/followers/', :query => {:id => user_id})
+  end
+
+  def self.find_following_by(user_id)
+    get('api/v1/colleagues/following/', :query => {:id => user_id})
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,53 @@
+require 'httparty'
+
 class User
-
   include HTTParty
-  base_uri 'staging.sharepractice.com' #ENV["API_RESOURCE_URL"]
+  base_uri 'staging.sharepractice.com'
+  default_params :output => 'json'
+  format :json
 
-  def initialize(username, password)
-    http request ==> name, attributes
-    @username = username
-    @password = password
-    @first_name = 
-    @last_name =
-    @npi =
-    @phone =
-    @degree =
-    @specialty =
-    @website =
-    @location =
-    @history =
-    @verified =
-    @avatar_url =
+  def initialize(email, password)
+    @response = HTTParty.get('/api/v1/tokens?format=json', :query => {:email => email, :password => password})
+    @email =      email
+    @password =   password
+    @first_name = response[:first_name]
+    @last_name =  response[:last_name]
   end
+
+  def response
+    @response.inspect
+  end
+
+  def self.find_by_email(email)
+    HTTParty.get('/api/v1/users/', :query => {:email => email})
+  end
+end
+
+#user = User.new("janet@sharepractice.com", "password")
+user = User.find_by_email("janet@sharepractice.com")
+puts user.response
+
+    # @npi =        response[:npi]
+    # @phone =      response[:phone]
+    # @degree =     response[:degree]
+    # @specialty =  response[:specialty]
+    # @website =    response[:website]
+    # @location =   response[:location]
+    # @history =    response[:history]
+    # @verified =   response[:verified]
+    # @avatar_url = response[:avatar_url]
+
+  # def self.find_by_email(email)
+  #   get('/api/v1/users/', :query => {:email => email})
+  # end
+
+  # def self.find_followers_of(user_id)
+  #   get('api/v1/colleagues/followers/', :query => {:id => user_id})
+  # end
+
+  # def self.find_following_by(user_id)
+  #   get('api/v1/colleagues/following/', :query => {:id => user_id})
+  # end
 
   # def change_attributes(params)
   #   #call frontline
@@ -41,24 +70,4 @@ class User
   #   render :invite
   # end
 
-end
-
-#   def initialize(u, p)
-#     @auth = {:username => u, :password => p}
-#   end
-
-#   # which can be :friends, :user or :public
-#   # options[:query] can be things like since, since_id, count, etc.
-#   def timeline(which=:friends, options={})
-#     options.merge!({:basic_auth => @auth})
-#     self.class.get("/statuses/#{which}_timeline.json", options)
-#   end
-
-#   def post(text)
-#     options = { :body => {:status => text}, :basic_auth => @auth }
-#     self.class.post('/statuses/update.json', options)
-#   end
 # end
-
-# twitter = Twitter.new(config['email'], config['password'])
-# pp twitter.timeline
