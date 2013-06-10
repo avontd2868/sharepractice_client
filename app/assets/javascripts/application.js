060402c9c8bbd55a0ca00d922698684bff861fc9
janetchang <janetlaichang@gmail.com>
2013-06-10 15:59:01 -0700

    make add/create new disorders work

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,6 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require prototype
-//= require prototype_ujs
-//= require effects
-//= require dragdrop
-//= require controls
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
