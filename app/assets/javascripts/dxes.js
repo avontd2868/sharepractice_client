1a0b10ed7a41be3a88396c17a3f5970862c3b496
janetchang <janetlaichang@gmail.com>
2013-06-10 16:06:23 -0700

    edit search partial to render collection of dx results,  make delete work (view still need work), make show work (need to make show in same page instead of directing to html),  first attempt at update

---
+++ app/assets/javascripts/dxes.js
@@ -0,0 +1,57 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+//$(document).ready(function () {
+
+// $('.edit-treatment-search').live('keydown',function (e) {
+//         var keyCode = e.keyCode || e.which;
+
+//         if (keyCode == 9 || keyCode == 13) {
+//             $(this).parent().removeClass('open');
+//             $(this).data('cui', "");
+//             // call custom function here
+//         }
+// });
+
+//$('.edit-button').click(function){
+//live("click", function () {
+        //make the disorder edit form show
+        //$(this).('.disorder-edit').removeClass('hidden');
+//$('.disorder-edit').removeClass('hidden');
+//};
+//};
+//         $(this).parent().addClass('info');
+//         $('#edit-prescription').empty();
+//         $('#prescriptions').empty();
+
+
+// function 
+
+// call something after the page is finished loading
+
+// $function
+
+
+// function edit_listener onClick 
+
+// know which row you're editting
+
+//     <div class="disorder edit hidden">
+//       <%= form_tag(dx_path(dx), :remote => true, :method => "Put") do %>
+//         <%= text_area_tag("message", nil, :class => 'message') %>
+//         <%= submit_tag("Submit changes", :class => 'btn btn-primary') %>
+//       <% end %>
+
+//       <%= form_tag(dx_path(dx), :remote => true, :method => "Put", :class => 'cancel') do %>
+//         <%= submit_tag("Cancel", :class => 'btn btn-danger action') %>
+//       <% end %>
+//     </div>
+
+
+// when the page finishes loading
+
+// create new item for class
+
+// .live use that class, new item with that class, link to that function to call
+
--- app/controllers/dxes_controller.rb
+++ app/controllers/dxes_controller.rb
@@ -2,7 +2,8 @@ class DxesController < ApplicationController
   
   # GET /dxes
   def index
-    @dxes = Dx.all
+    @all_dxes = Dx.all
+    @search_result_dxes
     @dx = Dx.new
   end
 
@@ -17,33 +18,42 @@ class DxesController < ApplicationController
     respond_to do |format|
       if @dx.save
         format.js
+        #render json: @dx, status: :created, location: @dxes
       else
         render 'index'
       end
     end
   end
 
+  def edit
+    @dx = Dx.find(params[:id])
+  end
+
   # PUT /dxes/1
-  # def update
-  #   @dx = Dx.find(params[:id])
+  def update
+    @dx = Dx.find(params[:id])
 
-  #   respond_to do |format|
-  #     if @dx.update_attributes(params[:dx])
-  #       format.json { head :no_content }
-  #     else
-  #       render 'error'
-  #     end
-  #   end
-  # end
+    respond_to do |format|
+      if @dx.update_attributes(params[:dx])
+        format.json { head :no_content }
+      else
+        render 'error'
+      end
+    end
+  end
 
-  # DELETE /dxes/1
-  # def destroy
-  #   @dx = Dx.find(params[:id])
-  #   @dx.destroy
+  def show
+    @dx = Dx.find(params[:id])
+  end
 
-  #   respond_to do |format|
-  #     format.html { redirect_to dxes_url }
-  #     format.json { head :no_content }
-  #   end
-  # end
+ 
+  def destroy
+    @dx = Dx.find(params[:id])
+    @dx.destroy
+
+    respond_to do |format|
+      #format.html { redirect_to dxes_url }
+      format.js
+    end
+  end
 end
